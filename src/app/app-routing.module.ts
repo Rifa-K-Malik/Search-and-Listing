d2e15b7c4d44db6a49a33d08a1c6e0470e379feb
Rifa-K-Malik <malikrif07@gmail.com>
2020-12-16 23:50:36 +0530

    Completed Search And Listing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { ListingComponent } from './components/listing/listing.component';
-import { SearchComponent } from './components/search/search.component';
+
 
 const routes: Routes = [];
 
