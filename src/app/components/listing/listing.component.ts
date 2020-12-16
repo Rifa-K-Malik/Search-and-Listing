cc424cb927bd92b25f5977c75a65ba1eb90bf606
Rifa-K-Malik <malikrif07@gmail.com>
2020-12-16 10:39:52 +0530

    Updated: Listing Layout (Click event)

--- src/app/components/listing/listing.component.ts
+++ src/app/components/listing/listing.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listing',
   templateUrl: './listing.component.html',
   styleUrls: ['./listing.component.scss']
 })
-export class ListingComponent implements OnInit {
+export class ListingComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input()
+  isActive: boolean;
+  onClick(){
+    this.isActive = !this.isActive;
   }
 
 }
